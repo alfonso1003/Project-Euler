0247fdebf63401ebd22396474480c343c3ba2aa4
alfonso1003 <alfonso.m.hernandez@gmail.com>
2015-02-23 13:46:09 -0600

    changed range to xrange, commited .project and .pydevproject files for pydev/eclipse

--- Python/Euler0001.py
+++ Python/Euler0001.py
@@ -5,4 +5,4 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
 
-print sum(x for x in range(0,1000) if x % 3 == 0 or x % 5 == 0)
+print sum(x for x in xrange(0,1000) if x % 3 == 0 or x % 5 == 0)
