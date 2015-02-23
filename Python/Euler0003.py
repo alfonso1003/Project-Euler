ef4bedfb6adc2f65d26300dd52f3ec62b1d2704c
alfonso1003 <alfonso.m.hernandez@gmail.com>
2015-02-23 17:49:42 -0600

    commit full solution for problem 3

--- Python/Euler0003.py
+++ Python/Euler0003.py
@@ -3,20 +3,40 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 """
+
 import math
 
 def find_factors(n):
     factors = []
     max = int(math.floor(math.sqrt(n)))
-    for x in range(1, max):
+    for x in range(1, max + 1):
         if n % x == 0:
             factors.append(x)
-            factors.append(n / x)
+            
+            if n != x:
+                factors.append(n / x)
         
     return sorted(factors)
 
-
 def is_prime(n):
-    return None
+    """
+    assummes n is non zero positive integer
+    """
+    
+    if n == 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    num_of_factors = len(find_factors(n))    
+    
+    if num_of_factors == 2:
+        return True
+    else:
+        return False
 
 factors = find_factors(600851475143)
+
+print max(f for f in factors if is_prime(f) == True)
